09c4c1a7b853c1a9526fdc7aff9b84f9f337eff2
dkchip <98587991+dkchip@users.noreply.github.com>
2023-02-08 23:34:18 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/trang-chu', (req, res) => res.send("hello world!"));
+
+app.listen(port,()=>{console.log(`{EX at https://localhost:${port}}`)})
